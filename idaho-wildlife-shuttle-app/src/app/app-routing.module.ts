5cd20b67a2d06f3ddac4e321ceaad4181ab8b292
William Grubin <williamgrubin@gmail.com>
2022-01-28 13:36:49 -0800

    Added elements to Login page, created and added elements to Register page

--- idaho-wildlife-shuttle-app/src/app/app-routing.module.ts
+++ idaho-wildlife-shuttle-app/src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
   }
 ];
 
