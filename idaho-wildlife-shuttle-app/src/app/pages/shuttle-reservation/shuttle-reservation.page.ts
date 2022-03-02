ddb0182add48972d8bdf6f0f11e380219e0fac2d
William Grubin <williamgrubin@gmail.com>
2022-03-02 11:46:17 -0800

    Added basic price casting from shuttle-reservation.page to pay-pal-web.page and added checkbox list for separate packages for tours.

--- idaho-wildlife-shuttle-app/src/app/pages/shuttle-reservation/shuttle-reservation.page.ts
+++ idaho-wildlife-shuttle-app/src/app/pages/shuttle-reservation/shuttle-reservation.page.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PayPalWebPageModule } from '../pay-pal-web/pay-pal-web.module';
+import { PayPalWebPage } from '../pay-pal-web/pay-pal-web.page';
+import { AppState } from 'src/store/AppState';
 
 @Component({
   selector: 'app-shuttle-reservation',
   templateUrl: './shuttle-reservation.page.html',
   styleUrls: ['./shuttle-reservation.page.scss'],
 })
+
 export class ShuttleReservationPage implements OnInit {
 
   constructor(private router: Router) { }
+
+  public form = [
+      { package: 'A', description: 'White Cloud Wilderness out of Salmon River -- 0.5-day shuttle -- Drop-off and pickup', 
+      price: '250.00', isChecked: false },
+      { package: 'B', description: 'Sawtooth National Forest through Frank Church Wilderness -- 1.5-day tour -- Drop-off and pickup shuttle', 
+      price: '500.00', isChecked: false },
+      { package: 'C', description: 'Snake River to Bear Creek -- 3-day backpacking trip -- Point-to-point shuttle', 
+      price: '1000.00', isChecked: false }
+    ];  
+
+  isClicked: false;
+  
+  cost: string = "250.00";
 
   ngOnInit() {
   }
@@ -17,8 +34,14 @@ export class ShuttleReservationPage implements OnInit {
     this.router.navigate(['home']);
   }
 
+  change(cost) {
+    this.cost = "500.00"
+  }
+
   pay_now(){
-    this.router.navigate(['pay-pal-web'])
+    PayPalWebPage.cost = this.cost;
+
+    this.router.navigate(['pay-pal-web']);
   }
   
 }
