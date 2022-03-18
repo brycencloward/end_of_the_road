95440e2172fc1bf26f3f470c4c723580d7464030
William Grubin <williamgrubin@gmail.com>
2022-03-18 16:37:06 -0700

    Minor adjustments to the appId to allow the app to be simulated on devices while signed under the LC development team.

--- idaho-wildlife-shuttle-app/capacitor.config.ts
+++ idaho-wildlife-shuttle-app/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.wpgrubin.shuttle',
+  appId: 'com.LCstate.shuttle',
   appName: 'idaho-wildlife-shuttle-app',
   webDir: 'www',
   bundledWebRuntime: false
