c9dbaec8dc561251d2f180718520ad0f89a8b2a2
Jay Myers <jtmyers@lmcail.lcsc.edu>
2022-04-13 11:01:23 -0700

    changed map in shuttle reservations added new page

--- idaho-wildlife-shuttle-app/src/app/app-routing.module.ts
+++ idaho-wildlife-shuttle-app/src/app/app-routing.module.ts
@@ -45,11 +45,6 @@ const routes: Routes = [
     loadChildren: () => import('./pages/cancelpolicy/cancelpolicy.module').then( m => m.CancelpolicyPageModule)
   },
   {
-    path: 'paypal-mobile',
-    loadChildren: () => import('./pages/paypal-mobile/paypal-mobile.module').then( m => m.PaypalMobilePageModule)
-    , canLoad: [AuthGuard]
-  },
-  {
     path: 'movecar',
     loadChildren: () => import('./pages/movecar/movecar.module').then( m => m.MovecarPageModule)
   }
